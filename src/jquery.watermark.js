170eebcafab178535d9735a679a9224f6dd683ab
Matthew Eagar <matthew.eagar@mosaic.com>
2011-11-10 14:23:57 -0500

    Move bind/unbind into separate functions

--- src/jquery.watermark.js
+++ src/jquery.watermark.js
@@ -24,6 +24,54 @@
  */
 (function($) {
 
+  function bindWatermark($box, options) {
+    if (options['hide_label'] && options['hide_label'])
+      options['label'].hide();
+
+    if (options['set_title'])
+      $box.attr('title', options['watermark']);
+
+
+    if (!$box.val() || $box.hasClass('empty')) {
+      $box.val(options['watermark']);
+      $box.addClass('empty');
+    } else if ($box.val() === options['watermark'] && !options['accept_watermark']) {
+      $box.addClass('empty');
+    }
+
+    $box.bind('focus.watermark', function() {
+      if ($(this).hasClass('empty')) {
+        $(this).val('').removeClass('empty');
+      }
+    });
+
+    $box.bind('blur.watermark', function() {
+      var options = $(this).data('watermark-options');
+
+      if (!$(this).val() || $(this).val() == options['watermark']) {
+        // no value specified on blur
+        $(this).addClass('empty');
+        $(this).val(options['watermark']);
+      } else {
+        // We have a value
+        $(this).removeClass('empty');
+      }
+    });
+  };
+
+  function unbindWatermark($box, options) {
+    if ($box.hasClass('empty')) {
+      // This field is currently "empty", discard its value
+      $box.val('');
+    }
+
+    $box.unbind('.watermark').removeClass('empty');
+
+    if (options['label'] && options['hide_label'])
+      options['label'].show();
+  }
+
+
 	$.fn.watermark = function(watermark, options) {
 
     if (!options) {
@@ -38,63 +86,32 @@
     options = $.extend({ }, options);
 
 		$(this).each(function() {
-			var $box = $(this), altText = null;
+			var $box = $(this), $label = null;
 
       if (watermark === false) {
-        if ($box.hasClass('empty')) {
-          // This field is currently "empty", discard its value
-          $box.val('')
-        }
-        $box.unbind('.watermark');
+        unbindWatermark($box, options);
         return;
       } else if (typeof(watermark) === 'string') {
 				// We've been given an explicit watermark
-				altText = watermark;
+				options['watermark'] = watermark;
 			} else {
 				// We weren't given a watermark, attempt to guess
-				var label = $('label[for=' + $box.attr('id') + ']');
+				$label = $('label[for=' + $box.attr('id') + ']');
 
-				if (label.size()) {
-          if (options['hide_label'])
-            label.hide();
-					altText = label.text();
+				if ($label.size()) {
+          options['label'] = $label;
+					options['watermark'] = $label.text();
 				} else if ($box.attr('title')) {
-					altText = $box.attr('title');
+					options['watermark'] = $box.attr('title');
 				} else {
 					alert('Unable to determine watermark for field ' + $box.attr('id'));
-					altText = '';
+          return;
 				}
 			}
 
-			$box.data('watermark-text', altText);
+			$box.data('watermark-options', options);
 
-			if (!$box.val() || $box.hasClass('empty')) {
-				$box.val(altText);
-				$box.addClass('empty');
-			} else if ($box.val() === altText && !options['accept_watermark']) {
-				$box.addClass('empty');
-			}
-
-			$box.attr('title', altText);
-
-		  $box.bind('focus.watermark', function() {
-        if ($(this).hasClass('empty')) {
-          $(this).val('').removeClass('empty');
-        };
-      });
-
-      $box.bind('blur.watermark', function() {
-        var watermark_text = $(this).data('watermark-text');
-
-        if (!$(this).val() || $(this).val() == watermark_text) {
-          // no value specified on blur
-          $(this).addClass('empty');
-          $(this).val(watermark_text);
-        } else {
-          // We have a value
-          $(this).removeClass('empty');
-        }
-      });
+      bindWatermark($box, options);
 		});
 
 		return this;
